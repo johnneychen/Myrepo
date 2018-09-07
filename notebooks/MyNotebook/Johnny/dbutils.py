007a4f511ed06f02d3f4354f5306bf999e63ea1c
johnneychen <chenjing957@163.com>
2018-09-07 15:01:22 +0800

    This is my first time sync up to Git

---
+++ notebooks/MyNotebook/Johnny/dbutils.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+dbutils.help()
+
+# COMMAND ----------
+
+dbutils.fs.help()
+
+# COMMAND ----------
+
+dbutils.notebook.help()
+
+# COMMAND ----------
+
+dbutils.widgets.help()
+
+# COMMAND ----------
+
+dbutils.secrets.help()
